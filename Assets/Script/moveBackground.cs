d3fd24ec028f6ec16bd756188fed2a3916f025d0
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-06-05 08:06:50 +0900

    배경 구현
    -배경 구현
    -속도를 다르게 주어 입체감이 느껴지게 함

---
+++ Assets/Script/moveBackground.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBackground : MonoBehaviour
+{
+    public GameObject Cloud;
+    public GameObject Factory;
+    public GameObject Factory2;
+    public GameObject Factory3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
